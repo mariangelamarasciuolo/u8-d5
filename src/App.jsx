eab0414a6e06f37e357591c7914c53f1303241f4
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-09-08 18:35:42 +0200

    spostato i film list

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import TopBar from "./components/TopBar";
 import Jumbo from "./components/Jumbo";
 import Footer from "./components/Footer";
 import FilmList from "./components/FilmList";
-import FilmListDue from "./components/FilmListDue";
-import FilmListTre from "./components/FilmListTre";
 
 function App() {
   return (
     <div className="App bg-dark text-light">
       <TopBar />
       <Jumbo />
-      <FilmList />
-      <FilmListDue />
-      <FilmListTre />
+      <FilmList movieTitle="harry%20potter" />
+      <FilmList movieTitle="fast%20furious" />
+      <FilmList movieTitle="breaking%20bad" />
+
       <Footer />
     </div>
   );
--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -7,22 +7,23 @@ class FilmList extends Component {
   };
 
   componentDidMount = () => {
-    this.HarryPotter();
+    this.getMovies();
   };
 
-  HarryPotter = async () => {
+  getMovies = async () => {
     try {
-      const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=2e445e77&s=harry%20potter");
+      const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=2e445e77&s=" + this.props.movieTitle);
       const data = await response.json();
-      const HarryPotter = data.Search;
+      const moviesArray = data.Search;
       console.log(data.Search);
-      this.setState({ films: HarryPotter });
+      this.setState({ films: moviesArray });
     } catch (error) {
       console.log(error);
     }
   };
 
   render() {
+    console.log(this);
     return (
       <div className="container-fluid px-4 mx-3">
         <h4 className="d-flex justify-content-start">Trending Now</h4>
@@ -32,7 +33,7 @@ class FilmList extends Component {
         >
           {this.state.films.map((film) => {
             return (
-              <div className="col mb-2 px-1">
+              <div className="col mb-2 px-1" key={film.imdbID}>
                 <img
                   className="img-fluid d-flex "
                   style={{ height: "20em", width: "18em" }}
